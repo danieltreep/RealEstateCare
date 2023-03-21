5c10bad18db1db2e3bf4c9c46f9cf9f6b79bbcb7
DanielTreep <danieltreep@live.nl>
2023-03-21 10:27:06 +0100

    added JSON

---
+++ src/components/CheckedReportsList.vue
@@ -0,0 +1,23 @@
+<template>
+    <ion-list>
+        <ion-item>
+            <ion-label>Inspectie 22-04-2022</ion-label>
+        </ion-item>
+        <ion-item>
+            <ion-label>Inspectie 12-04-2022</ion-label>
+        </ion-item>
+        <ion-item>
+            <ion-label>Inspectie 24-03-2022</ion-label>
+        </ion-item>
+        <ion-item>
+            <ion-label>Inspectie 21-03-2022</ion-label>
+        </ion-item>
+        <ion-item>
+            <ion-label>Inspectie 20-03-2022</ion-label>
+        </ion-item>
+    </ion-list>
+</template>
+<script setup lang="ts">
+import { IonItem, IonLabel, IonList } from '@ionic/vue'; 
+
+</script>
--- src/views/CheckedReports.vue
+++ src/views/CheckedReports.vue
@@ -3,13 +3,19 @@
         <HeaderSection />
         <ion-content :fullscreen="true">
             <h1>Uitgevoerde Rapporten</h1>
-
+            <CheckedReportsList />
       
         </ion-content>
     </ion-page>
 </template>
 <script setup lang="ts">
     import HeaderSection from '@/components/HeaderSection.vue';
+    import CheckedReportsList from '@/components/CheckedReportsList.vue';
     import { IonPage, IonContent } from '@ionic/vue'
 
 </script>
+<style>
+    h1 {
+        text-align: center;
+    }
+</style>
